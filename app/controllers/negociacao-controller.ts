aeb77e149e50f7fc9ab39dca008deb84c5ac8859
Lucas Reis <luk.wik40@gmail.com>
2023-06-17 16:04:42 -0300

    Criado modelo de negociações, um array que armazena as negociações cadastradas. Foi implementado também Readonly para que não haja alteração do array, como o método pop, a única alteração é através do método interno adiciona()

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,9 +1,11 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 
 export class NegociacaoController {
   private inputData: HTMLInputElement;
   private inputQuantidade: HTMLInputElement;
   private inputValor: HTMLInputElement;
+  private negociacoes = new Negociacoes();
 
   constructor() {
     this.inputData = document.querySelector("#data");
@@ -11,12 +13,26 @@ export class NegociacaoController {
     this.inputValor = document.querySelector("#valor");
   }
 
-  adiciona() {
+  adiciona(): void {
+    const negociacao = this.criaNegociacao();
+    this.negociacoes.adiciona(negociacao);
+    console.log(this.negociacoes.lista());
+    this.limparFormulario();
+  }
+
+  criaNegociacao(): Negociacao {
     const exp = /-/g;
-    const date = new Date(this.inputData.value.replace(exp, ','));
-    const quantidade = parseInt(this.inputQuantidade.value)
-    const valor = parseFloat(this.inputValor.value)
-    const negociacao = new Negociacao(date, quantidade, valor);
-    console.log(negociacao);
+    const date = new Date(this.inputData.value.replace(exp, ","));
+    const quantidade = parseInt(this.inputQuantidade.value);
+    const valor = parseFloat(this.inputValor.value);
+    return new Negociacao(date, quantidade, valor);
+  }
+
+  limparFormulario(): void {
+    this.inputData.value = "";
+    this.inputQuantidade.value = "";
+    this.inputValor.value = "";
+
+    this.inputData.focus();
   }
 }
